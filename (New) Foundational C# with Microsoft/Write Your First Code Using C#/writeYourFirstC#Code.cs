c3c582b700d5f85dd05d828d9169da9609c7f0da
Shivakumar-001 <shivagana2004@gmail.com>
2024-10-24 21:13:18 +0530

    completed 1st module

---
+++ (New) Foundational C# with Microsoft/Write Your First Code Using C#/writeYourFirstC#Code.cs
@@ -0,0 +1,7 @@
+/* Go through the provided module create a new acount in learn.micorosft.com or if you already have an account 
+start with sign in then start Learning*/
+/*The answer for this question is 
+
+Console.WriteLine appends a new line after the output.
+
+*/
