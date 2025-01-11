14712a5cdb9980677090529b3ee6280a80022df6
Shivakumar-001 <shivagana2004@gmail.com>
2025-01-11 20:42:27 +0530

    Building a Trivia bot in js

---
+++ Certified Full Stack Developer Curriculum/JAVASCRIPT/lab-javascript-trivia-bot.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Catty";
+let botLocation = "Bengaluru";
+let favoriteLanguage = "Javascript";
+console.log("My name is "+botName+" and I live on "+botLocation+".");
+console.log("My favorite programming language is "+favoriteLanguage+".");
+let codingFact = "Codig is easy if you learn "+favoriteLanguage;
+console.log(codingFact);
+codingFact = "If you want to become a software developer you should know "+favoriteLanguage;
+console.log(codingFact);
+codingFact = "Learn "+favoriteLanguage+". It is easy to learn.";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+".");
