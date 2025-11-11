222e9b6cba3a9f07d27bcca2308aba4edeb8fed6
Shivakumar-001 <shivagana2004@gmail.com>
2025-11-11 06:59:42 +0530

    Building a number pattern generatot

---
+++ Certified Full Stack Developer Curriculum/Python/build-a-number-pattern-generator.py
@@ -0,0 +1,13 @@
+** start of main.py **
+
+def number_pattern(n):
+    if not isinstance(n, int):
+        return "Argument must be an integer value."
+    if n <= 0:
+        return "Argument must be an integer greater than 0."
+    return ' '.join(map(str, range(1, n + 1)))
+
+
+
+** end of main.py **
+
