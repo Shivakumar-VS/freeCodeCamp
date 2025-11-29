23a7ffc75f3053a4cf7a0c46c87378017dcfb40e
Shivakumar-001 <shivagana2004@gmail.com>
2025-11-29 12:50:34 +0530

    Building a reusable footerr

---
+++ Certified Full Stack Developer Curriculum/React/build_a_reusable_footer.jsx
@@ -0,0 +1,25 @@
+export const Footer = () => {
+  return (
+    <footer>
+  <div class="footer-container">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Services</a></li>
+    </ul>
+
+    <ul>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Team</a></li>
+    </ul>
+
+    <ul>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Support</a></li>
+    </ul>
+
+    <p>© 2025 YourWebsite. All rights reserved.</p>
+  </div>
+</footer>
+
+  )
+};
